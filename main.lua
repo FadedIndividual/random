48f4e568316ee691195307ebfa24c432f85d92fb
FadedIndividual <141731826+FadedIndividual@users.noreply.github.com>
2024-07-06 05:27:17 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -18,7 +18,7 @@ else
 			["Blox-Fruits"] = {["PID"] = {"7449423635", "4442272183", "2753915549"}; ["On-Teleport"] = false; ["Raw"] = "https://raw.githubusercontent.com/REDzHUB/BloxFruits/main/redz9999"};
 			--["LPI"] = {["On-Teleport"] = false; ["Raw"] = nil; ["Extra"] = {}};
 			["DH"] = {["PID"] = {"417267366"}; ["On-Teleport"] = false; ["Raw"] = "https://raw.githubusercontent.com/FadedIndividual/MAIN-Serverhopping-Bot/main/Main.lua"};
-			["PF"] = {["PID"] = {"292439477"}; ["On-Teleport"] = false; ["Raw"] = "https://raw.githubusercontent.com/FadedIndividual/random/main/pf.lua"}
+			["PF"] = {["PID"] = {"292439477"}; ["On-Teleport"] = false; ["Raw"] = "https://raw.githubusercontent.com/FadedIndividual/random/main/pf.lua"};
 		};
 	}--[[ 2753915549/Blox-Fruits - - 292439477/PF - - 391104146/LPI - - 417267366/DH ]]
 	Update()
@@ -30,10 +30,11 @@ local b11, b55 = Add.Category_Button("Settings")
 local FFPPSS = Add.t_TextBox(b11, "FPS Cap ["..tostring(Main_T.Vars.Client.FPS).."]", function(Tbox) if tonumber(Tbox.Text) and (tonumber(Tbox.Text)>= 10 and tonumber(Tbox.Text) <= 999) then Main_T.Vars.Client.FPS = tonumber(Tbox.Text) Tbox.PlaceholderText = "FPS Cap ["..tostring(Main_T.Vars.Client.FPS).."]"; Update() setfpscap(tonumber(Tbox.Text)) end end, 4)
 local INFY = Add.b_Button(b11, "Infinite Yield", function(but) spawn(function() loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))() end) but:Destroy() end, false)
 spawn(function()
-	while task.wait()
-	if IY_LOADED or _G.IY_DEBUG then
-		INFY:Destroy()
-	end
+	while task.wait() do
+        if IY_LOADED or _G.IY_DEBUG then
+            INFY:Destroy()
+        end
+    end
 end)
 
 local BSS1, BSS2 = nil, nil;
