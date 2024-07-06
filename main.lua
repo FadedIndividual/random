4a88bc7ffcd7d5285282afc6c5450b0dc6bee38e
FadedIndividual <141731826+FadedIndividual@users.noreply.github.com>
2024-07-06 05:25:37 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,7 +2,7 @@ repeat task.wait() until game.Loaded and game:GetService("Players") and game.Pla
 
 local Players = game:GetService("Players")
 local Local = Players.LocalPlayer
-local NAME, VERSION = "social_area", 6
+local NAME, VERSION = "social_area", 7
 local Main_T = {}
 local function Update() writefile(NAME..".txt", game:GetService("HttpService"):JSONEncode(Main_T)) end
 
@@ -18,8 +18,9 @@ else
 			["Blox-Fruits"] = {["PID"] = {"7449423635", "4442272183", "2753915549"}; ["On-Teleport"] = false; ["Raw"] = "https://raw.githubusercontent.com/REDzHUB/BloxFruits/main/redz9999"};
 			--["LPI"] = {["On-Teleport"] = false; ["Raw"] = nil; ["Extra"] = {}};
 			["DH"] = {["PID"] = {"417267366"}; ["On-Teleport"] = false; ["Raw"] = "https://raw.githubusercontent.com/FadedIndividual/MAIN-Serverhopping-Bot/main/Main.lua"};
+			["PF"] = {["PID"] = {"292439477"}; ["On-Teleport"] = false; ["Raw"] = "https://raw.githubusercontent.com/FadedIndividual/random/main/pf.lua"}
 		};
-	}--[[ 2753915549/Blox-Fruits - - 391104146/LPI - - 417267366/DH ]]
+	}--[[ 2753915549/Blox-Fruits - - 292439477/PF - - 391104146/LPI - - 417267366/DH ]]
 	Update()
 end setfpscap(tonumber(Main_T.Vars.Client.FPS))
 
@@ -28,6 +29,12 @@ local Add = loadstring(game:HttpGet("https://raw.githubusercontent.com/FadedIndi
 local b11, b55 = Add.Category_Button("Settings")
 local FFPPSS = Add.t_TextBox(b11, "FPS Cap ["..tostring(Main_T.Vars.Client.FPS).."]", function(Tbox) if tonumber(Tbox.Text) and (tonumber(Tbox.Text)>= 10 and tonumber(Tbox.Text) <= 999) then Main_T.Vars.Client.FPS = tonumber(Tbox.Text) Tbox.PlaceholderText = "FPS Cap ["..tostring(Main_T.Vars.Client.FPS).."]"; Update() setfpscap(tonumber(Tbox.Text)) end end, 4)
 local INFY = Add.b_Button(b11, "Infinite Yield", function(but) spawn(function() loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))() end) but:Destroy() end, false)
+spawn(function()
+	while task.wait()
+	if IY_LOADED or _G.IY_DEBUG then
+		INFY:Destroy()
+	end
+end)
 
 local BSS1, BSS2 = nil, nil;
 for i, v in pairs(Main_T["Settings"]) do
