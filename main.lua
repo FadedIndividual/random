28f85136205525bd678fdf427489d0ce3702c8b5
FadedIndividual <141731826+FadedIndividual@users.noreply.github.com>
2024-07-06 18:57:45 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,134 +1,50 @@
-repeat task.wait() until game.Loaded; task.wait(2.5)
+repeat task.wait() until game.Loaded and game:GetService("Players") and game.Players.LocalPlayer; wait(2.5)
 
-Players = game:GetService("Players")
-LocalPlayer = Players.LocalPlayer
-Mouse = LocalPlayer:GetMouse()
-UIS = game:GetService("UserInputService")
-RunService = game:GetService("RunService")
+local Players = game:GetService("Players")
+local Local = Players.LocalPlayer
+local NAME, VERSION = "social_area", 7
+local Main_T = {}
+local function Update() writefile(NAME..".txt", game:GetService("HttpService"):JSONEncode(Main_T)) end
 
-local num, CC, _Folders, _Cache, _Locked = 2, workspace.CurrentCamera, {}, {}, nil
+if isfile(NAME..".txt") and game:GetService("HttpService"):JSONDecode(readfile(NAME..".txt")).Version == VERSION  then
+	Main_T = game:GetService("HttpService"):JSONDecode(readfile(NAME..".txt"))
+else
+	Main_T = {
+		["Version"] = VERSION;
+		["Vars"] = {
+			["Client"] = {["FPS"] = 30};
+		};
+		["Settings"] = {
+			["Blox-Fruits"] = {["PID"] = {"7449423635", "4442272183", "2753915549"}; ["On-Teleport"] = false; ["Raw"] = "https://raw.githubusercontent.com/REDzHUB/BloxFruits/main/redz9999"};
+			--["LPI"] = {["On-Teleport"] = false; ["Raw"] = nil; ["Extra"] = {}};
+			["DH"] = {["PID"] = {"417267366"}; ["On-Teleport"] = false; ["Raw"] = "https://raw.githubusercontent.com/FadedIndividual/MAIN-Serverhopping-Bot/main/Main.lua"};
+			["PF"] = {["PID"] = {"292439477"}; ["On-Teleport"] = false; ["Raw"] = "https://raw.githubusercontent.com/FadedIndividual/random/main/pf.lua"};
+		};
+	}--[[ 2753915549/Blox-Fruits - - 292439477/PF - - 391104146/LPI - - 417267366/DH ]]
+	Update()
+end setfpscap(tonumber(Main_T.Vars.Client.FPS))
 
-local function GetTeam(Part) 
-    for i,Vest in pairs(Part:GetDescendants()) do
-        if Vest.ClassName == "MeshPart" and Vest.MeshId == "rbxassetid://11232478007" and Vest.BrickColor == BrickColor.new("Earth blue") then
-            return game:GetService("Teams").Phantoms;
-        end
-    end
+local Add = loadstring(game:HttpGet("https://raw.githubusercontent.com/FadedIndividual/random/main/lib.lua"))()
 
-    return game:GetService("Teams").Ghosts;
-end
-
-local function GetChars()
-    local Teams, num = {["Ghosts"] = {};["Phantoms"] = {}}, 0
-    
-    if workspace:FindFirstChild("Players") and #workspace.Players:GetChildren() >= 2 then
-        pcall(function()
-            for _,z in pairs(workspace.Players:GetChildren()) do
-                if tostring(GetTeam(z:GetChildren()[1])) == "Ghosts" then
-                    Teams.Ghosts = z:GetChildren()
-                else
-                    Teams.Phantoms = z:GetChildren()
-                end
-            end
-        end)
-    end
-
-    return Teams
-end
-function GetGUN()
-    local gun;
-
-    if #CC:GetChildren() >= 2 then
-        for _,z in pairs(CC:GetChildren()) do
-            if not z:FindFirstChild("Arms") and z:IsA("Model") then
-                gun = z
-            end
-        end
-    end
-
-    return gun
-end
-function _Heads()
-    local TBLL = {}
-    if _Folders then
-        for iii, zz in pairs(_Folders) do
-            if tostring(iii) ~= tostring(LocalPlayer.Team) then
-                for ii, vv in pairs(zz) do
-					for _, v in pairs(vv:GetDescendants()) do
-						if v:IsA("BasePart") and #v:GetChildren() == 2 and v:FindFirstChildOfClass("Decal") and v:FindFirstChildOfClass("SpecialMesh") and v:FindFirstChildOfClass("SpecialMesh").MeshId == "rbxassetid://6179256256" then
-							table.insert(TBLL, v)
-						end
-					end
-                end
-            end
-        end
-    end
-    if #TBLL == 0 then return end
-    return TBLL
-end
-PositionToScreen = function(Vectorf)
-    local Vector, OnScreen = workspace.CurrentCamera:WorldToScreenPoint(Vectorf)
-    return Vector, OnScreen
-end
-GetClosestMouse = function(TBall)
-    local Closest
-    local MaxDistance = 50
-    for _,Head in next, TBall do
-		local ScreenPosition, OnScreen = PositionToScreen(Head.Position)
-		local Distance = (Vector2.new(Mouse.X, Mouse.Y) - Vector2.new(ScreenPosition.X, ScreenPosition.Y)).Magnitude
-		if OnScreen and Distance <= MaxDistance then
-			Closest = Head
-			MaxDistance = Distance
-		end
-    end
-    if Closest then
-        return Closest
-    else
-        return nil
-    end
-end
-
-game:GetService("RunService").Stepped:Connect(function()
-    _Folders = GetChars()
-    if _Folders then
-		pcall(function()
-            if not UIS:IsMouseButtonPressed(0) then
-			    _Locked = GetClosestMouse(_Heads())
-            end
-		end)
-        for i, v in pairs(_Folders) do
-            if tostring(i) == tostring(LocalPlayer.Team) then
-            else
-                if #v >= 1 then
-                    for _,z in pairs(v) do
-                        if not z:FindFirstChild("x_x") then
-                            local High = Instance.new("Highlight")
-                            High.Parent = z; High.Name = "x_x"; High.Adornee = z; High.OutlineColor = Color3.fromRGB(255, 0, 0) High.OutlineTransparency = .25; High.FillTransparency = 1; High.Enabled = true; High.LineThickness = 3; High.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
-                            table.insert(_Cache, High)
-                        end
-                    end
-                end
-            end
-        end
-    end
-    if _Locked and UIS:IsMouseButtonPressed(1) then
-        local vec = PositionToScreen(_Locked.Position)
-        mousemoverel((vec.X-Mouse.X), (vec.Y-Mouse.Y))
-    end
-    if _Cache and #_Cache >= 1 then
-        for i, v in pairs(_Cache) do
-            v.OutlineColor = Color3.fromHSV(tick()%1, 1, 1)
-        end
-    end
-    local gun = GetGUN() if gun == nil then return end
-
-    if gun:FindFirstChild("LaserDot") then
-        gun.LaserDot.BillboardGui.ImageLabel.ImageColor3 = Color3.fromHSV(tick()%1, 1, 1)
-    end
-    for i, v in pairs(gun:GetDescendants()) do
-        if v:IsA("BasePart") and v.Transparency <= .99 then
-            v.Transparency = .9
-            v.Color = Color3.fromHSV(tick()%1, 1, 1)
+local b11, b55 = Add.Category_Button("Settings")
+local FFPPSS = Add.t_TextBox(b11, "FPS Cap ["..tostring(Main_T.Vars.Client.FPS).."]", function(Tbox) if tonumber(Tbox.Text) and (tonumber(Tbox.Text)>= 10 and tonumber(Tbox.Text) <= 999) then Main_T.Vars.Client.FPS = tonumber(Tbox.Text) Tbox.PlaceholderText = "FPS Cap ["..tostring(Main_T.Vars.Client.FPS).."]"; Update() setfpscap(tonumber(Tbox.Text)) end end, 4)
+local INFY = Add.b_Button(b11, "Infinite Yield", function(but) spawn(function() loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))() end) but:Destroy() end, false)
+spawn(function()
+	while task.wait() do
+        if IY_LOADED or _G.IY_DEBUG then
+            INFY:Destroy()
         end
     end
 end)
+
+local BSS1, BSS2 = nil, nil;
+for i, v in pairs(Main_T["Settings"]) do
+	if v["PID"] and table.find(v["PID"], tostring(game.PlaceId)) then
+		if v["On-Teleport"] then
+			loadstring(game:HttpGet(tostring(v.Raw)))()
+		else
+			spawn(function() BSS2 = Add.b_Button(b11, tostring(i), function(but) spawn(function() loadstring(game:HttpGet(tostring(v.Raw)))() end) but:Destroy() end, false) end)
+		end
+		spawn(function() BSS1 = Add.b_Button(b11, (tostring(i) .. ": " .. tostring(v["On-Teleport"])), function(button) v["On-Teleport"] = not v["On-Teleport"]; button.Text = (tostring(i) .. ": " .. tostring(v["On-Teleport"])); Update() end, false) end)
+	end
+end
