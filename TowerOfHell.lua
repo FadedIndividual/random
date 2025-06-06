7b76bd819e6fbf330a434c30d0e0cfe902a2e305
FadedIndividual <141731826+FadedIndividual@users.noreply.github.com>
2025-06-06 04:18:27 -0700

    Create TowerOfHell.lua

---
+++ TowerOfHell.lua
@@ -0,0 +1,365 @@
+repeat task.wait() until game:IsLoaded(); wait(2)
+
+local Add = loadstring(game:HttpGet("https://raw.githubusercontent.com/FadedIndividual/main/refs/heads/main/Library.lua"))()
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+local Mouse = LocalPlayer:GetMouse()
+local UIS = game:GetService("UserInputService")
+local RunService = game:GetService("RunService")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local TweenService = game:GetService("TweenService")
+local Camera = workspace.CurrentCamera
+local Core = game:GetService("CoreGui")
+
+local GGName = "@nyu_. made this xoxo"
+local sg = Instance.new("ScreenGui") sg.IgnoreGuiInset = true; sg.Name = GGName
+local gethui = protectgui or gethui or get_hidden_gui
+
+if gethui then
+    if gethui():FindFirstChild(GGName) then
+        gethui():FindFirstChild(GGName):Destroy()
+    end
+    sg.Parent = gethui()
+else
+    if Core:FindFirstChild(GGName) then
+        Core:FindFirstChild(GGName):Destroy()
+    end
+    sg.Parent = Core
+end
+
+local MainSize, TopYSize, Hue, Round, nUm = UDim2.new(0, 500, 0, 400), 25, Color3.fromRGB(0, 120, 255), UDim.new(0, 8), 0
+local isRecording = false
+local Main = Add.Frame(sg, MainSize, UDim2.new(.5, -MainSize.X.Offset/2, .5, -MainSize.Y.Offset/2), {["BackgroundTran"] = 0.1, ["Draggable"] = true})
+local TopSelec = Add.Frame(Main, UDim2.new(1, 0, 0, TopYSize), UDim2.new(0, 0, 0, 0), {["BackgroundTran"] = 1})
+Add:Extra(TopSelec, {"uill", {["HorizonAlign"] = Enum.HorizontalAlignment.Center, ["FillDir"] = Enum.FillDirection.Horizontal}})
+Add:Extra(Main, {"corner", {["rad"] = Round}})
+Add:Extra(TopSelec, {"corner", {["rad"] = Round}})
+
+local last = nil
+local function Create_Category(Text) nUm += 1
+    local bFrame = Add.Frame(Main, UDim2.new(1, 0, 1, -TopYSize), UDim2.new(0, 0, 0, TopYSize), {["Name"] = "Category_Frame", ["BackgroundTran"] = 1})
+    Add:Extra(bFrame, {"uill", {["HorizonAlign"] = Enum.HorizontalAlignment.Center, ["FillDir"] = Enum.FillDirection.Vertical}})
+    local bMain = Add.Button(TopSelec, UDim2.new(1, 0, 0, TopYSize-3), UDim2.new(0, 0, 0, 0), Text, {["BackgroundColor"] = Color3.new(0, 0, 0), ["BackgroundTran"] = 1, ["Font"] = Enum.Font.Code, ["TextSize"] = TopYSize-6, ["LayoutOrder"] = nUm})
+    local AMT = 0; for i,v in ipairs(TopSelec:GetChildren()) do if v:IsA("TextButton") then AMT += 1 end end for i,v in ipairs(TopSelec:GetChildren()) do if v:IsA("TextButton") then v.Size = UDim2.new(1/AMT, 0, 1, 0) end end
+
+    if last then
+        last = Add.Frame(bMain, UDim2.new(0, 1, 1, 0), UDim2.new(1, 0, 0, 0), {["BackgroundTran"] = .8, ["BackgroundColor"] = Hue})
+        bFrame.Visible = false
+    else
+        last = Add.Frame(bMain, UDim2.new(0, 1, 1, 0), UDim2.new(1, 0, 0, 0), {["BackgroundTran"] = .8, ["BackgroundColor"] = Hue})
+        bMain.TextSize = TopYSize-4
+    end
+
+    bMain.MouseButton1Click:Connect(function()
+        task.spawn(function()
+            local tt2 = Add.Frame(bMain, UDim2.new(0, 0, 0, 0), UDim2.new(0.5, 0, 0.5, 0), {["BackgroundTran"] = 1, ["BackgroundColor"] = Hue}) Add:Extra(tt2, {"corner", {["rad"] = Round}})
+            TweenService:Create(tt2, TweenInfo.new(.21, Enum.EasingStyle.Linear, Enum.EasingDirection.In), {BackgroundTransparency = .7}):Play()
+            TweenService:Create(tt2, TweenInfo.new(.19, Enum.EasingStyle.Linear, Enum.EasingDirection.In), {Size = UDim2.new(1, 0, 1, 0)}):Play()
+            TweenService:Create(tt2, TweenInfo.new(.19, Enum.EasingStyle.Linear, Enum.EasingDirection.In), {Position = UDim2.new(0, 0, 0, 0)}):Play()
+            task.wait(.2)
+            tt2:Destroy()
+        end)
+        for i,v in pairs(Main:GetChildren()) do
+            if v.Name == "Category_Frame" then
+                v.Visible = false
+            end
+        end
+        for i,v in ipairs(TopSelec:GetChildren()) do
+            if v:IsA("TextButton") then
+                v.TextSize = TopYSize-6
+            end
+        end
+        bMain.TextSize = TopYSize-4
+        bFrame.Visible = true
+    end)
+
+    return bFrame, bMain
+end
+
+local function Button(Parent, Text, Function, Type) nUm += 1
+    local bMain = Add.Button(Parent, UDim2.new(1, 0, 0, TopYSize-3), UDim2.new(0, 0, 0, 0), "", {["BackgroundColor"] = Color3.new(0, 0, 0), ["BackgroundTran"] = .75, ["TextSize"] = TopYSize-6, ["LayoutOrder"] = nUm})
+    if Type == 1 then
+        local line = Add.Text(bMain, UDim2.new(1, 0, 0, 1), UDim2.new(0, 0, 1, -1), "", {["BackgroundColor"] = Hue, ["BackgroundTran"] = .8})
+        bMain.Text = Text
+        bMain["TextXAlignment"] = Enum.TextXAlignment.Center
+        bMain.MouseButton1Click:Connect(function()
+            task.spawn(function()
+                local tt2 = Add.Frame(bMain, UDim2.new(0, 0, 1, 0), UDim2.new(0, 0, 0, 0), {["BackgroundTran"] = 1, ["BackgroundColor"] = Hue}) Add:Extra(tt2, {"corner", {["rad"] = Round}})
+                TweenService:Create(tt2, TweenInfo.new(.25, Enum.EasingStyle.Linear, Enum.EasingDirection.In), {BackgroundTransparency = 0.8}):Play()
+                TweenService:Create(tt2, TweenInfo.new(.25, Enum.EasingStyle.Linear, Enum.EasingDirection.In), {Size = UDim2.new(1, 0, 1, 0)}):Play()
+                TweenService:Create(tt2, TweenInfo.new(.25, Enum.EasingStyle.Linear, Enum.EasingDirection.In), {Position = UDim2.new(0, 0, 0, 0)}):Play()
+                task.wait(.27)
+                tt2:Destroy()
+            end)
+            Function(bMain)
+        end)
+    elseif Type == 2 then
+        local mFrame = Add.Frame(Parent, UDim2.new(1, 0, 0, TopYSize-3), UDim2.new(0, 0, 0, 0), {["BackgroundTran"] = 1, ["LayoutOrder"] = nUm})
+        local mLabe = Add.Text(mFrame, UDim2.new(.8, 0, 1, 0), UDim2.new(0, 0, 0, 0), "", {["BackgroundTran"] = .75, ["BackgroundColor"] = Color3.new(0, 0, 0), ["TextSize"] = TopYSize-6})
+        local line = Add.Text(mLabe, UDim2.new(1, 0, 0, 1), UDim2.new(0, 0, 1, -1), "", {["BackgroundColor"] = Hue, ["BackgroundTran"] = .8})
+        bMain.Parent = mFrame
+        bMain.Size = UDim2.new(.2, 0, 1, 0)
+        bMain.Position = UDim2.new(.8, 0, 0, 0)
+        if type(Text) ~= "string" then
+            mLabe.Text = Text[1]
+            bMain.Text = Text[2]
+        else
+            mLabe.Text = Text
+            bMain.Text = "..."
+        end
+        bMain.BackgroundTransparency = .5
+        bMain.MouseButton1Click:Connect(function()
+            Function(bMain, mLabe)
+        end)
+    end
+
+    return bMain
+end
+
+local function Box(Parent, Text, Function, Type) nUm += 1
+    local mBox = Add.Box(Parent, UDim2.new(1, 0, 0, TopYSize-3), UDim2.new(0, 0, 0, 0), "", {["BackgroundColor"] = Color3.new(0, 0, 0), ["BackgroundTran"] = .75, ["TextSize"] = TopYSize-6, ["LayoutOrder"] = nUm})
+    if Type == 1 then
+        local line = Add.Text(mBox, UDim2.new(1, 0, 0, 1), UDim2.new(0, 0, 1, -1), "", {["BackgroundColor"] = Hue, ["BackgroundTran"] = .8})
+        mBox.PlaceholderText = Text
+        mBox.FocusLost:Connect(function()
+            Function(mBox)
+        end)
+    elseif Type == 2 then
+        local mFrame = Add.Frame(Parent, UDim2.new(1, 0, 0, TopYSize-3), UDim2.new(0, 0, 0, 0), {["BackgroundTran"] = 1, ["LayoutOrder"] = nUm})
+        mBox.Parent = mFrame
+        mBox.Size = UDim2.new(.2, 0, 1, 0)
+        mBox.Position = UDim2.new(.8, 0, 0, 0)
+        mBox.BackgroundTransparency = .5
+        local mBut = Button(mFrame, Text, function()
+            Function(mBox, button)
+        end, 1)
+		if type(Text) ~= "string" then
+            mBut.Text = Text[1]
+			mBox.PlaceholderText = Text[2]
+        else
+            mBut.Text = Text
+            bMain.Text = "..."
+        end
+        mBut.Size = UDim2.new(.8, 0, 1, 0)
+    elseif Type == 3 then
+        local mFrame = Add.Frame(Parent, UDim2.new(1, 0, 0, TopYSize-3), UDim2.new(0, 0, 0, 0), {["BackgroundTran"] = 1, ["LayoutOrder"] = nUm})
+        local mLabe = Add.Text(mFrame, UDim2.new(.8, 0, 1, 0), UDim2.new(0, 0, 0, 0), "", {["BackgroundTran"] = .75, ["BackgroundColor"] = Color3.new(0, 0, 0), ["TextSize"] = TopYSize-6})
+        local line = Add.Text(mLabe, UDim2.new(1, 0, 0, 1), UDim2.new(0, 0, 1, -1), "", {["BackgroundColor"] = Hue, ["BackgroundTran"] = .8})
+        mBox.Parent = mFrame
+        mBox.Size = UDim2.new(.2, 0, 1, 0)
+        mBox.Position = UDim2.new(.8, 0, 0, 0)
+        mBox.BackgroundTransparency = .5
+        mBox.FocusLost:Connect(function()
+            Function(mBox, mLabe)
+        end)
+		if type(Text) ~= "string" then
+            mLabe.Text = Text[1]
+			mBox.PlaceholderText = Text[2]
+        else
+            mLabe.Text = Text
+            bMain.Text = "..."
+        end
+    end
+end
+
+local loc = Create_Category("Local")
+
+local WSPEED, JPOWER = 16, 50
+local saPButton = Box(loc, {"WalkSpeed:", "16"}, function(box, text)
+    local nm = tonumber(box.Text)
+    if nm then
+        box.PlaceholderText = tostring(nm)
+        box.Text = ""
+        WSPEED = nm
+    else
+        box.PlaceholderText = "16"
+        box.Text = ""
+        WSPEED = 16
+    end
+end, 3)
+
+local jpWButton = Box(loc, {"JumpPower:", "50"}, function(box, text)
+    local nm = tonumber(box.Text)
+    if nm then
+        box.PlaceholderText = tostring(nm)
+        box.Text = ""
+        JPOWER = nm
+    else
+        box.PlaceholderText = "50"
+        box.Text = ""
+        JPOWER = 50
+    end
+end, 3)
+
+local DoubleJumpAmonut = 0
+local djjButton = Box(loc, {"Double-Jump Amount:", "0"}, function(box, text)
+    local nm = tonumber(box.Text)
+    if nm then
+        box.PlaceholderText = tostring(nm)
+        box.Text = ""
+        DoubleJumpAmonut = nm
+    else
+        box.PlaceholderText = "0"
+        box.Text = ""
+        DoubleJumpAmonut = 0
+    end
+end, 3)
+
+local DKScript = false
+local aasda1 = Button(loc, "Disable Kill Bricks: false", function(button)
+	DKScript = not DKScript
+    button.Text = "Disable Kill Bricks: " .. tostring(DKScript)
+end, 1)
+
+local AFObby = false
+local abas = Button(loc, "Auto Farm: false", function(button)
+	AFObby = not AFObby
+    button.Text = "Auto Farm: " .. tostring(AFObby)
+	if AFObby then
+		if not DKScript then
+			DKScript = true
+			aasda1.Text = "Disable Kill Bricks: " .. tostring(DKScript)
+		end
+	end
+end, 1)
+
+local SB_Frame = Add.Frame(sg, UDim2.new(0, 200, 0, 30), UDim2.new(.5, -100, 1, -200), {})
+Add:Extra(SB_Frame, {"UICorner", {}})
+local SB_Slide = Add.Frame(SB_Frame, UDim2.new(0, 0, 1, 0), UDim2.new(0, 0, 0, 0), {["BackgroundTran"] = .5, ["BackgroundColor"] = Color3.new(0, 1, 0)})
+Add:Extra(SB_Slide, {"UICorner", {}})
+
+SB_Frame.Visible = false
+
+local tpBind, dbDela = Enum.KeyCode.Z, tick()
+local TPbd2 = Button(loc, {"Speed-Boost:", "Z"}, function(button, text)
+    isRecording = true
+    task.wait(.3)
+    UIS.InputBegan:Once(function(Key, Proc)
+        if Proc then return end
+
+        tpBind = Key.KeyCode
+        button.Text = string.gsub(tostring(Key.KeyCode), "Enum.KeyCode.", "")
+
+        task.wait(1)
+        isRecording = false
+    end)
+end, 2)
+
+local spbAMT, deLaAy = 10, .1
+local sbiButton = Box(loc, {"Speed-Boost Amount:", "10"}, function(box, text)
+    local nm = tonumber(box.Text)
+    if nm then
+        box.PlaceholderText = tostring(nm)
+        box.Text = ""
+        spbAMT = nm
+    else
+        box.PlaceholderText = "10"
+        box.Text = ""
+        spbAMT = 10
+    end
+end, 3)
+
+local abas = Button(loc, "Visualize Speed-Delay: false", function(button)
+	SB_Frame.Visible = not SB_Frame.Visible
+    button.Text = "Visualize Speed-Delay: " .. tostring(SB_Frame.Visible)
+end, 1)
+
+local bind = Enum.KeyCode.LeftAlt
+local bt2 = Button(loc, {"Close Menu:", "LeftAlt"}, function(button, text)
+    isRecording = true
+    task.wait(.3)
+    UIS.InputBegan:Once(function(Key, Proc)
+        if Proc then return end
+
+        bind = Key.KeyCode
+        button.Text = string.gsub(tostring(Key.KeyCode), "Enum.KeyCode.", "")
+
+        task.wait(.3)
+        isRecording = false
+    end)
+end, 2)
+
+UIS.InputBegan:Connect(function(Key, Proc)
+    if Proc or isRecording then return end
+
+    if Key.KeyCode == bind then
+        Main.Visible = not Main.Visible
+	elseif Key.KeyCode == tpBind then
+        local humanoid = LocalPlayer.Character and LocalPlayer.Character:FindFirstChildOfClass("Humanoid")
+        local hrp = LocalPlayer.Character and LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
+        if hrp and (tick()-dbDela)>= deLaAy then
+            dbDela = tick()
+            local moveDir = humanoid.MoveDirection
+            if moveDir.Magnitude > 0 then
+                hrp.CFrame = hrp.CFrame + (moveDir.Unit * spbAMT)
+            else
+                hrp.CFrame = hrp.CFrame * CFrame.Angles(0, math.rad(0), 0) * CFrame.new(0, 0, -spbAMT)
+            end
+        end
+    end
+end)
+
+last:Destroy()
+
+local Connect = nil
+local function NoVeloToggle(Bool)
+	if Bool then
+		Connect = RunService.Stepped:Connect(function()
+			for i,v in pairs(LocalPlayer.Character:GetChildren()) do
+				if v:IsA("BasePart") then
+					v.Velocity = Vector3.zero
+					v.RotVelocity = Vector3.zero
+				end
+			end
+		end)
+	else
+		pcall(function()
+			Connect:Disconnect()
+		end)
+	end
+end
+
+task.spawn(function()
+	while task.wait() do
+		local hrp = LocalPlayer.Character and LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
+		if hrp and AFObby then
+			local Center = workspace:FindFirstChild("tower") and workspace:FindFirstChild("tower"):FindFirstChild("sectionData") and workspace:FindFirstChild("tower"):FindFirstChild("sectionData"):FindFirstChild("finish") and workspace:FindFirstChild("tower"):FindFirstChild("sectionData"):FindFirstChild("finish"):FindFirstChild("center")
+			if Center then
+				NoVeloToggle(true)
+				local Tween = TweenService:Create(hrp, TweenInfo.new(25, Enum.EasingStyle.Linear), {CFrame = Center.CFrame})
+				Tween:Play()
+				for i = 1, 25 do
+					if AFObby then
+						task.wait(1)
+					else
+						Tween:Cancel()
+						break
+					end
+				end
+				NoVeloToggle(false)
+			end
+		end
+	end
+end)
+
+RunService.Stepped:Connect(function()
+	local numm = math.clamp(tick()-dbDela, 0, deLaAy)
+    SB_Slide.Size = UDim2.new(numm*(1/deLaAy), 0, 1, 0)
+    if numm == deLaAy then
+        SB_Slide.BackgroundTransparency = 0
+		SB_Slide.BackgroundColor3 = Color3.new(0, 1, 0)
+    else
+        SB_Slide.BackgroundTransparency = .55
+		SB_Slide.BackgroundColor3 = Color3.new(1, 0, 0)
+    end
+	
+	ReplicatedStorage.GameValues.globalJumps.Value = DoubleJumpAmonut
+	ReplicatedStorage.GameValues.killbricksDisabled.Value = DKScript
+
+	local human = LocalPlayer.Character and LocalPlayer.Character:FindFirstChildOfClass("Humanoid")
+	
+	if human then
+		human.WalkSpeed = WSPEED
+		human.JumpPower = JPOWER
+	end
+end)
